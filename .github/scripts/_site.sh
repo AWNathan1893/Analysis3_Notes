6c50bc75f4714ef4f2445223a1921595d6175f8b
BikramHalder <bikram0x@outlook.com>
2023-08-22 10:11:28 +0530

    Add last update feature

--- .github/scripts/_site.sh
+++ .github/scripts/_site.sh
@@ -3,14 +3,14 @@
 echo "Creating _site/"
 mkdir _site
 
+echo "Add time for last update: $(date +'%A, %b %d %Y, %I:%M %p')"
+echo -e "\n_Last updated: $(date +'%A, %b %d %Y, %I:%M %p')_" >>".github/index.md"
+
 echo "Compiling .github/index.md to _site/index.html"
 pandoc --katex .github/index.md -s -o _site/index.html
 
 echo "Zipping pdfs to Analysis-3_all_pdfs.zip"
-zip -r Analysis-3_all_pdfs.zip pdfs/*
+zip -r _site/Analysis-3_all_pdfs.zip pdfs/*
 
 echo "Copying pdfs to _site/pdfs"
 cp -r pdfs _site/pdfs
-
-echo "Copying Analysis-3_all_pdfs.zip to _site/Analysis-3_all_pdfs.zip"
-cp Analysis-3_all_pdfs.zip _site/Analysis-3_all_pdfs.zip
