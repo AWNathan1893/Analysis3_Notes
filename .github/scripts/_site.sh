27b640ab057f6b7c8a13c2273060800a29c55f55
BikramHalder <bikram0x@outlook.com>
2023-08-22 10:38:25 +0530

    Specify date & time in IST

--- .github/scripts/_site.sh
+++ .github/scripts/_site.sh
@@ -4,7 +4,7 @@ echo "Creating _site/"
 mkdir _site
 
 echo "Add time for last update: $(date +'%A, %b %d %Y, %I:%M %p')"
-echo -e "\n_Last updated: $(date +'%A, %b %d %Y, %I:%M %p')_" >>".github/index.md"
+echo -e "\n_Last updated: $(TZ='Asia/Kolkata' date +'%A, %b %d %Y, %I:%M %p') IST_" >>".github/index.md"
 
 echo "Compiling .github/index.md to _site/index.html"
 pandoc --katex .github/index.md -s -o _site/index.html
